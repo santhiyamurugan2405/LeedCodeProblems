ee0a539db139eb8468dd4755583abcdef5b457a8
Santhiya <santhiyamurugan2405@gmail.com>
2026-07-22 21:31:17 +0530

    Time: 4 ms (100.00%) | Memory: 46.1 MB (16.94%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num=x;
+        int rev=0;
+        while(num<0){
+            return false;
+        }
+        while(num>0){
+            int digit=num%10;
+            rev=rev*10+digit;
+            num/=10;
+        }
+        if(x==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
